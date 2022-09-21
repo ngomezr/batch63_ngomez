a423c841c672b0fe96163fbdaf0962013b223353
ngomezr <47580268+ngomezr@users.noreply.github.com>
2022-09-21 08:58:30 -0700

    commit

--- Day2_App1/src/Sample.java
+++ Day2_App1/src/Sample.java
@@ -8,11 +8,28 @@
  *	
  * 
  */
+
+class A {
+	// Where ever static is avalible its loading
+	int x = 200;
+	static int y = 100;
+	
+	void show() {
+		System.out.println( x + " " + y);
+		x++; y++;
+	}
+}
+
 public class Sample {
 
 	public static void main(String[] args) {
 	
 		System.out.println("Hello World");
+		
+		A a1 = new A(); a1.show();
+		A a2 = new A(); a2.show();
+		A a3 = new A(); a3.show();
+		
 	}
 
 }
